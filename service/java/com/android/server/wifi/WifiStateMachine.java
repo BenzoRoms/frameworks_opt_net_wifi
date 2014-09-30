7ac729cb2d5a14a39b18e23627f7ff22a973b3a3
Harshavardhan Nalajala <hnalajala@nvidia.com>
2014-09-29 17:22:01 -0700

    Get link layer stats only after wifi init
    Sometimes, if screen intent is received during wifi
    hal initialization, segmentation fault error is hit.
    This is due to direct native call getWifiLinkLayerStats.
    
    Change-Id: I970b3254b89a4b4758a1a0342ca73b5185733e32

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -3569,8 +3569,7 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
             }
         }
         mScreenBroadcastReceived.set(true);
-
-        getWifiLinkLayerStats(false);
+        sendMessage(CMD_GET_LINK_LAYER_STATS);
         mOnTimeScreenStateChange = mOnTime;
         lastScreenStateChangeTimeStamp = lastLinkLayerStatsUpdate;
 

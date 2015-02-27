55732ce3ee0c8bc79eeaeec89b1f9e72b3c1f016
Pontus Fuchs <pontus2.fuchs@sonymobile.com>
2015-02-27 09:53:45 +0100

    wifi: Don't call wpa_supplicant from binder thread context
    Commands to wpa_supplicant are serialized through WifiStateMachine.
    This rule is broken when dumpstate collects data from the wifi
    service. In this case a command will be sent wpa_supplicant from the
    binder thread.
    
    If wifi is turned off at the same time as dumpstate is running the
    wpa control socket structure can be cleared leading to a crash
    in system server.
    
    Since the info from the dump is not of high value we can remove it.
    
    Change-Id: I4f829d017176261272b479cef9dcb0e10baca345

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -2629,7 +2629,6 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
         pw.println("mOperationalMode " + mOperationalMode);
         pw.println("mUserWantsSuspendOpt " + mUserWantsSuspendOpt);
         pw.println("mSuspendOptNeedsDisabled " + mSuspendOptNeedsDisabled);
-        pw.println("Supplicant status " + mWifiNative.status(true));
         pw.println("mLegacyPnoEnabled " + mLegacyPnoEnabled);
         pw.println("mDriverSetCountryCode " + mDriverSetCountryCode);
         pw.println("mConnectedModeGScanOffloadStarted " + mConnectedModeGScanOffloadStarted);
